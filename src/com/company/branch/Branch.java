f08fb9b02e4b4dab809c53a6aab38f0a6d70b383
Kazimieras Keršulis <kazimieras.kersulis@gmail.com>
2018-02-21 21:26:37 +0200

    added tree population method and initial calculator class

---
+++ src/com/company/branch/Branch.java
@@ -0,0 +1,21 @@
+package com.company.branch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Branch {
+
+    List<Branch> branches;
+
+    public Branch() {
+        branches = new ArrayList<Branch>();
+    }
+
+    public void addBranch(Branch branch) {
+        branches.add(branch);
+    }
+
+    public List<Branch> getBranches() {
+        return branches;
+    }
+}
---
+++ src/com/company/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.company.calculator;
+
+import com.company.branch.Branch;
+
+public class Calculator {
+
+    public static int calculateMaxDepth(Branch branch) {
+
+        int maxDepth = 0;
+
+        return maxDepth;
+    }
+}
