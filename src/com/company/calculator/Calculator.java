9b1ca24cbe063107ee10725dd649c69f02327d01
Kazimieras Kersulis <kazimieras.kersulis@relativity.com>
2018-02-22 14:20:45 +0200

    added calculateMaxDepth() method implementation

--- src/com/company/calculator/Calculator.java
+++ src/com/company/calculator/Calculator.java
@@ -4,10 +4,25 @@ import com.company.branch.Branch;
 
 public class Calculator {
 
-    public static int calculateMaxDepth(Branch branch) {
+    public static int calculateMaxDepth(Branch branch, int depth) {
 
+        int branchDepth = depth;
         int maxDepth = 0;
 
+        // tikriname ar šaka turi savyje šakų
+        if(branch.getBranches().isEmpty()) {
+        } else {
+
+            for (int i = 0; i < branch.getBranches().size(); i++) {
+                //System.out.println(branch.getBranches().get(i).getBranches().size());
+                branchDepth++;
+                //System.out.println("Branch depth for " + branchDepth);
+                calculateMaxDepth(branch.getBranches().get(i), branchDepth);
+            }
+            maxDepth = branchDepth;
+        }
+
         return maxDepth;
+
     }
 }
