4e8c4a5a4b7f72949d91530d315ebb3e8e585c5d
Kazys Keršulis <kazimieras.kersulis@gmail.com>
2018-02-25 23:17:54 +0200

    updated CalculateTreeHeight method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,10 +24,9 @@ public class Main {
         branch.getBranches().get(1).addBranch(new Branch());
 
         // add new branch to the last left branch
-        //branch.getBranches().get(0).getBranches().get(0).addBranch(new Branch());
-        branch.getBranches().get(1).getBranches().get(0).addBranch(new Branch());
+        branch.getBranches().get(0).getBranches().get(0).addBranch(new Branch());
 
-        // The result should be : 3
+        // The result should be : 4
         System.out.println("The max depth of the tree is " + Calculator.calculateTreeHeight(branch));
     }
 }
