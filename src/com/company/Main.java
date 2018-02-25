85962dfaa1fc98add630bb73b94484e679e2c500
Kazys Keršulis <kazimieras.kersulis@gmail.com>
2018-02-25 20:53:05 +0200

    updated calculateTreeDepth() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,15 +17,15 @@ public class Main {
 
         // add new branches to the left branch
         branch.getBranches().get(0).addBranch(new Branch());
-       // branch.getBranches().get(0).addBranch(new Branch());
-       // branch.getBranches().get(0).addBranch(new Branch());
+        branch.getBranches().get(0).addBranch(new Branch());
+        branch.getBranches().get(0).addBranch(new Branch());
 
         // add new branches to the right branch
-        //branch.getBranches().get(1).addBranch(new Branch());
+        branch.getBranches().get(1).addBranch(new Branch());
 
         // add new branch to the last left branch
-        //branch.getBranches().get(0).getBranches().get(0).addBranch(new Branch());
-        //branch.getBranches().get(1).getBranches().get(0).addBranch(new Branch());
+        branch.getBranches().get(0).getBranches().get(0).addBranch(new Branch());
+        branch.getBranches().get(1).getBranches().get(0).addBranch(new Branch());
 
         System.out.println("The max depth of the tree is " + calculator.traverseTree(branch));
     }
--- src/com/company/calculator/Calculator.java
+++ src/com/company/calculator/Calculator.java
@@ -6,26 +6,30 @@ public class Calculator {
 
     int maxDepth = 0;
 
-
     public int traverseTree(Branch branch) {
 
+        // set initial branch level as 0
         maxDepth = calculateTreeDepth(branch, 0);
         return maxDepth;
     }
 
     private int calculateTreeDepth(Branch branch, int depth) {
+
         int branchDepth = depth;
 
         for (int i = 0; i < branch.getBranches().size(); i++) {
-            branchDepth++;
+
             if (!(branch.getBranches().isEmpty())) {
 
-                calculateTreeDepth(branch.getBranches().get(i), branchDepth);
+                branchDepth++;
 
                 if (branchDepth > maxDepth) {
                     maxDepth = branchDepth;
                 }
 
+                calculateTreeDepth(branch.getBranches().get(i), branchDepth);
+
+                break;
             }
         }
         return maxDepth;
